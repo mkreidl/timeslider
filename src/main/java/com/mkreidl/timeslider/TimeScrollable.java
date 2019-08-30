8bc4252a2850199c40bf97daede94ca3ae911a5c
Martin Kreidl <martin.kreidl.privat@gmail.com>
2019-08-31 00:33:50 +0200

    Migrate to SDK 29, androidX

--- src/main/java/com/mkreidl/timeslider/TimeScrollable.java
+++ src/main/java/com/mkreidl/timeslider/TimeScrollable.java
@@ -1,7 +1,7 @@
 package com.mkreidl.timeslider;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.Locale;
 import java.util.TimeZone;
